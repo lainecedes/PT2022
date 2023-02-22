177188f442c7a590a3ea88872b9b6b0d536a79cb
lainecedes <elaine.parieh@gmail.com>
2023-02-22 17:42:46 +0100

    routes

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-import chalk from 'chalk';
-import express from 'express';
-
-// const express = require('express');
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 1337;
-// const chalk = require('chalk');
 
-app.get('/', onHome).listen(PORT, console.log(chalk.magenta(`Running on port: ${PORT}`)));
+const chalk = require('chalk');
 
-function onHome(req, res) {
-  res.send('Hallo');
-}
+const port = 1337; // port
+
+app.use(express.static('static'));
+
+
+app.get('/', (req, res) => { // radiance homepagina
+  res.send('Hallo! Dit is de homepagina');
+})
+
+app.get('/filter', (req, res) => { //  filter pagina
+  res.send('Filter pagina');
+})
+
+app.get('/filter/results', (req, res) => { //  filter pagina + resultaten
+  res.send('Resultaten pagina van filters');
+})
+
+.listen(port,() => {
+console.log(chalk.magenta(`Running on port: ${port}`))
+})
