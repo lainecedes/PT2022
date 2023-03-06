f9c12c3ed9976181c246f604bc0283106344c2c0
lainecedes <elaine.parieh@gmail.com>
2023-03-06 16:03:26 +0100

    code progress

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require('express'); // express
-const app = express();
+const { engine } = require('express-handlebars'); // express-handlebars
+
 const chalk = require('chalk'); // chalk
 const path = require('path');
-const { engine } = require('express-handlebars'); // express-handlebars
+
 const port = 1337; // port
 
-app.use(express.static('static'));
+const app = express();
+app.use('/static', express.static('static'));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('views', './views');
 
+
+
 app.get('/', (req, res) => {  // radiance homepagina view
-    res.render('home');
-});
+    res.render('home.hbs'); // home.hbs
+})
+
+app.get('/filter-page', (req, res) => { //  filter pagina
+  res.render('filter-page.hbs');
+})
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
-});
+  console.log(chalk.magenta(`Running on port: ${port}`))
+})
 
-// app.get('/filter-open', (req, res) => { //  filter pagina
-//   res.send('Filter pagina open');
-// })
-
-// .listen(port,() => {
-// console.log(chalk.magenta(`Running on port: ${port}`))
-// })
