9c986394a03c20fab9356a0a47e694ffec4cf5c4
lainecedes <elaine.parieh@gmail.com>
2023-03-15 05:28:27 +0100

    done, need final check

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express'); // express
 const { engine } = require('express-handlebars'); // express-handlebars
 const chalk = require('chalk'); // chalk, for better view in terminal
 const path = require('path'); // path
-
 const fetch = require('node-fetch'); // node-fetch for API
 const app = express();
-
 const multer = require('multer'); // multer
 const upload = multer();
 
@@ -15,8 +13,6 @@ const port = process.env.PORT || 1337; // port
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded({extended: true})); // parses form data
-
-
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
@@ -28,7 +24,7 @@ app.set('views', './views');
 require('dotenv').config({path: '.env'}); // dotenv
 
 const { MongoClient, ServerApiVersion} = require('mongodb');
-const uri = process.env.DB_CONNECTION_STRING;
+const uri = process.env.DB_CONNECTION_STRING; 
 
 const client = new MongoClient (
  uri,
@@ -41,11 +37,9 @@ client.connect()
 
 
 
-
 // Connection of the database and its collection
 const radianceDatabase = client.db('radiance');
 const collection = radianceDatabase.collection('user');
-
 
 
 
@@ -70,7 +64,6 @@ app.get('/', (req, res) => {
           .catch(err => {
               console.log(`error ${err}`)
     }); 
-
 })
 
 
@@ -82,8 +75,9 @@ app.get('/add-profile', (req, res) => {
 
 // Page where confirmation of added profile is shown
 app.post('/add-profile-success', upload.any(), async(req, res) => {
-  res.render('add-profile-success.hbs', {title: 'Radiance - Profile added!'});
-  collection.insertOne(req.body);
+  let newProfile;
+  res.render('add-profile-success.hbs', {title: 'Radiance - Profile added!', data: newProfile});
+  newProfile = collection.insertOne(req.body);
   console.log(req.body);
 })
 
@@ -101,6 +95,14 @@ app.post('/filter-results', async(req, res) => {
 
   res.render('filter-results.hbs', {data: radianceUser, title: 'Radiance - Results'});
 })
+
+
+
+app.use((req, res) => { // error handler, style in css
+  res.status(404);
+  res.render('404.hbs', {title: 'Radiance - 404 page not found'});
+});
+
 
 
 // Port connection
